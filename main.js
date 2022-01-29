ce317efec03d234449006c0a85b17bf4ae45acca
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-01-29 15:08:10 +0530

    added conversions

--- main.js
+++ main.js
@@ -858,6 +858,10 @@ function mapRange (value, a, b, c, d) {
 function degToRad(deg) {
     return deg * Math.PI / 180;
 }
+
+function radToDeg(rad) {
+    return rad * 180 / Math.PI;
+}
 ///////////////////////////////////////////////////////////////////////////////
 
 ////////////// common mathematical functions ///////////////////////////////
